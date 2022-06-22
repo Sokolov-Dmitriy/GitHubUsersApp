31482eb4a75136f0e46ef3f765cdd098b301349e
Dmitriy-Sokolov <dmitriu9511@gmail.com>
2022-06-22 22:09:00 +0300

    добавил возможность использования без токена

--- data/src/main/java/com/sokolovds/data/cloudDataSource/CloudConfig.kt
+++ data/src/main/java/com/sokolovds/data/cloudDataSource/CloudConfig.kt
@@ -13,8 +13,9 @@ object CloudConfig {
         private val type: String = "Bearer",
         //create your Personal access tokens in GitHub Settings->Developer settings->Personal access tokens
         //choose only:"read:user, user:email"
+        //add your token in local.properties like: token = ghp_dds..ds (without quotation marks)
         private val token: String = BuildConfig.TOKEN,
-        val value: String = "$type $token"
+        val value: String = if (token != "null") "$type $token" else ""
     )
 
     val HEADER = Header()
